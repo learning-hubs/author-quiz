488d9d0237d882ec6f11423837d135b989b791c5
Suman Kumari <suman.kumari@metro-external.digital>
2026-02-24 12:13:50 +0530

    completed logic of selecting random books and according select an author by the books generated

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { sample, shuffle } from 'underscore';
 
 const authors = [
   {
@@ -11,12 +12,55 @@ const authors = [
     imageUrl: 'images/authors/marktwain.jpg',
     imageSource: 'Wikimedia Commons',
     books: ['The Adventures of Huckleberry Finn', 'Life on the missessippi', 'Roughing It']
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: 'images/authors/charlesdickens.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['David CopperField', 'A tale of two cities']
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/josephconrad.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Heart of Darkness']
+  },
+  {
+    name: 'J.K. Rowling',
+    imageUrl: 'images/authors/jkrowling.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Harry Porter And the Sorcerers Stone']
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: 'images/authors/stephenking.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Shining', 'IT']
+  },
+  {
+    name: 'William Shakespeare',
+    imageUrl: 'images/authors/shakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Macbeth', 'As you like it', 'Hamlet']
   }
 ];
 
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p, c, i) {
+    return p.concat(c.books);
+  }, []);
+
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find(author => author.books.some(title => title === answer))
+  }
+}
 const state = {
   turnData: {
-    author: authors[0],
+    author: getTurnData(authors).author,
     books: authors[0].books
   }
 }
